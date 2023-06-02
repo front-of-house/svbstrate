c97a58e08e29f4c2580f36ed97f9f1e82f71d6de
Eric Bailey <git@esb.lol>
2023-06-02 15:31:26 -0500

    feat: update build for core package

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,22 +1,6 @@
 import { defineConfig } from "vitest/config";
-import { resolve } from "path";
 
 export default defineConfig({
-  define: {
-    ENV: JSON.stringify("development"),
-  },
-  build: {
-    target: "esnext",
-    minify: "esbuild",
-    lib: {
-      entry: [
-        resolve(__dirname, "lib/index.ts"),
-        resolve(__dirname, "lib/presets.ts"),
-        resolve(__dirname, "lib/utils.ts"),
-      ],
-      formats: ["cjs"],
-    },
-  },
   test: {
     coverage: {
       reporter: ["text", "lcov"],
